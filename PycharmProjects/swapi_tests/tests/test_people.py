170ef2a419474f9c409ffa4da30e030ceb8183c4
alalion <alnpaiuk@gmail.com>
2021-02-11 13:21:16 +0200

    2nd commit

--- PycharmProjects/swapi_tests/tests/test_people.py
+++ PycharmProjects/swapi_tests/tests/test_people.py
@@ -3,7 +3,7 @@ from utils.utils import base_url
 from utils.utils import people
 from utils.utils import search
 from utils.utils import assert_attributes_type
-from utils.test_person_1 import *
+from utils.utils import test_person_1
 
 url = base_url + people
 
@@ -29,22 +29,22 @@ class TestPeople():
 
     def test_search_people(self):
         response = requests.get(url + search + 'Luke Skywalker')
-        assert response.json()['results'][0]['name'] == name
-        assert response.json()['results'][0]['height'] == height
-        assert response.json()['results'][0]['mass'] == mass
-        assert response.json()['results'][0]['hair_color'] == hair_color
-        assert response.json()['results'][0]['skin_color'] == skin_color
-        assert response.json()['results'][0]['eye_color'] == eye_color
-        assert response.json()['results'][0]['birth_year'] == birth_year
-        assert response.json()['results'][0]['gender'] == gender
-        assert response.json()['results'][0]['homeworld'] == homeworld
-        assert response.json()['results'][0]['films'] == films
-        assert response.json()['results'][0]['species'] == species
-        assert response.json()['results'][0]['vehicles'] == vehicles
-        assert response.json()['results'][0]['starships'] == starships
-        assert response.json()['results'][0]['created'] == created
-        assert response.json()['results'][0]['edited'] == edited
-        assert response.json()['results'][0]['url'] == person_url
+        assert response.json()['results'][0]['name'] == test_person_1['name']
+        assert response.json()['results'][0]['height'] == test_person_1['height']
+        assert response.json()['results'][0]['mass'] == test_person_1['mass']
+        assert response.json()['results'][0]['hair_color'] == test_person_1['hair_color']
+        assert response.json()['results'][0]['skin_color'] == test_person_1['skin_color']
+        assert response.json()['results'][0]['eye_color'] == test_person_1['eye_color']
+        assert response.json()['results'][0]['birth_year'] == test_person_1['birth_year']
+        assert response.json()['results'][0]['gender'] == test_person_1['gender']
+        assert response.json()['results'][0]['homeworld'] == test_person_1['homeworld']
+        assert response.json()['results'][0]['films'] == test_person_1['films']
+        assert response.json()['results'][0]['species'] == test_person_1['species']
+        assert response.json()['results'][0]['vehicles'] == test_person_1['vehicles']
+        assert response.json()['results'][0]['starships'] == test_person_1['starships']
+        assert response.json()['results'][0]['created'] == test_person_1['created']
+        assert response.json()['results'][0]['edited'] == test_person_1['edited']
+        assert response.json()['results'][0]['url'] == test_person_1['person_url']
 
     def test_search_people_by_partial_name(self):
         response = requests.get(url + search + 'anakin')
